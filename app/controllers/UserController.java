053f3a34be33bc102fd0324dd3a8cc160fa168da
addy360 <abbyboy360@gmail.com>
2022-02-10 10:30:40 +0300

    register link

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -10,12 +10,13 @@ import play.mvc.*;
 import javax.inject.Inject;
 import java.util.List;
 
-@With(AuthMiddleware.class)
+
 public class UserController extends Controller {
 
     @Inject
     FormFactory formFactory;
 
+    @With(AuthMiddleware.class)
     public Result index(Http.Request request){
         List<User> users = DB.find(User.class).findList();
         return  ok(views.html.users.index.render(request, users));
@@ -37,17 +38,20 @@ public class UserController extends Controller {
         return redirect(routes.UserController.index()).flashing("success","user has been inserted successfully!");
     }
 
+    @With(AuthMiddleware.class)
     public Result show(double id){
         User user = DB.find(User.class,id);
         return ok(views.html.users.show.render(user));
     }
 
+    @With(AuthMiddleware.class)
     public Result edit(double id, Http.Request request){
         Form<User> userForm = formFactory.form(User.class);
         User user = DB.find(User.class, id);
         return ok(views.html.users.edit.render(user, request, userForm));
     }
 
+    @With(AuthMiddleware.class)
     public Result update( double id, Http.Request request){
         User user = DB.find(User.class,id);
         Form<User> userForm = formFactory.form(User.class).bindFromRequest(request);
@@ -64,6 +68,7 @@ public class UserController extends Controller {
         return redirect(routes.UserController.index()).flashing("success","user updated successfully");
     }
 
+    @With(AuthMiddleware.class)
     public Result delete(double id){
         DB.delete(User.class, id);
         return redirect(routes.UserController.index());
